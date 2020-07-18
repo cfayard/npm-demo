773cd464aab7c96058de2fb347c88bd6b8cf906e
Casey Fayard <fayardcasey@gmail.com>
2020-07-18 13:45:31 -0400

    published and used new npm package

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var _ = require('underscore')
+// var _ = require("underscore");
 
-let result = _.contains([1,2,3], 2)
-console.log(result)
+// let result = _.contains([1, 2, 3], 2);
+// console.log(result);
+
+var casey = require("casey-fayard");
+var result = casey.multiply(6, 6);
+console.log(result);
